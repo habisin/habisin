7f705be16b58d1e43dd8600c700181cb7967441f
Kai Folf <git@fcd.im>
2025-11-28 15:48:12 +0700

    feat: add role to fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use \App\Models\Shop;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -20,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function shop(){
+        return $this->hasOne(Shop::class);
+    }
+
 }
